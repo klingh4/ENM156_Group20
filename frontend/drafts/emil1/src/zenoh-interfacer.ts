786d2bdb05b80edc2703102cdf820e26df9ecdcd
Emil Karlsson <karlsem@student.chalmers.se>
2025-12-05 08:54:07 +0100

    Communication with simulator

--- frontend/drafts/emil1/src/zenoh-interfacer.ts
+++ frontend/drafts/emil1/src/zenoh-interfacer.ts
@@ -1,24 +1,50 @@
-import { Config, Session, type Sample } from "@eclipse-zenoh/zenoh-ts";
+import { Config, Session, Subscriber, type Sample } from "@eclipse-zenoh/zenoh-ts";
 import { decodePayloadFromTypeName, uncover } from "@rise-maritime/keelson-js"
+import type { LocationFix } from '@rise-maritime/keelson-js/dist/payloads/index.foxglove'
+import type { TimestampedFloat } from '@rise-maritime/keelson-js/dist/payloads/Primitives';
+import { createOrMoveMarker } from "./map";
 
 const ZENOH_BRIDGE_REMOTE_API_DEFAULT_PORT = "10000";
-const SUB_KEY = "rise/@v0/@v0/MASS_0/pubsub/location_fix/gnss/0"
+const SOG_KEY = "rise/@v0/@v0/MASS_0/pubsub/speed_over_ground_knots/gnss/0"
+const COG_KEY = "rise/@v0/@v0/MASS_0/pubsub/course_over_ground_deg/gnss/0"
+const LOCATION_FIX_KEY = "rise/@v0/@v0/MASS_0/pubsub/location_fix/gnss/0"
 
 console.log("Starting session...");
 let session;
-let subscriber;
+let subscribers: Array<Subscriber> = [];
 export async function connect() {
     session = await Session.open(new Config("ws://localhost:" + ZENOH_BRIDGE_REMOTE_API_DEFAULT_PORT));
-    subscriber = await session.declareSubscriber(SUB_KEY, {
+    subscribers.push(await session.declareSubscriber(LOCATION_FIX_KEY, {
         handler: (sample: Sample) => {
-            console.log(`Recieved message of ${sample.encoding()} encoding`);
             let p = uncover(sample.payload().toBytes())?.[2]
             if (p) {
-                const loc = decodePayloadFromTypeName('foxglove.LocationFix', p);
-                console.log(loc);
+                const loc = decodePayloadFromTypeName('foxglove.LocationFix', p) as LocationFix;
+                createOrMoveMarker(loc.frameId, [loc.latitude, loc.longitude]);
             }
 
         }
-    });
-    console.log("Subscriber declared?");
-}
+    }));
+    const sogElem = document.getElementById("sog");
+    if (sogElem)
+        subscribers.push(await session.declareSubscriber(SOG_KEY, {
+            handler: (sample: Sample) => {
+                let p = uncover(sample.payload().toBytes())?.[2]
+                if (p) {
+                    const sogVal = decodePayloadFromTypeName('keelson.TimestampedFloat', p) as TimestampedFloat;
+                    sogElem.innerText = String(sogVal.value)
+                }
+
+            }
+        }));
+    const cogElem = document.getElementById("cog");
+    if (cogElem)
+        subscribers.push(await session.declareSubscriber(COG_KEY, {
+            handler: (sample: Sample) => {
+                let p = uncover(sample.payload().toBytes())?.[2]
+                if (p) {
+                    const cogVal = decodePayloadFromTypeName('keelson.TimestampedFloat', p) as TimestampedFloat;
+                    cogElem.innerText = String(cogVal.value);
+                }
+            }
+        }));
+};
